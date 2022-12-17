1d421ffe3436ecf804c08c647a5a9215b25563c8
Jordan Jueckstock <jpjuecks@bju.edu>
2022-12-17 10:30:43 -0500

    ot texts compiled, mostly

--- tgntools/__main__.py
+++ tgntools/__main__.py
@@ -4,15 +4,27 @@ import argparse
 import sys
 
 from .refs import parse_ref
-from .data import BibleBooks
+from .data import BibleBooks, BIBLE_FILE
 
+ap = argparse.ArgumentParser(description="Parse and typeset an edit list.")
+ap.add_argument("-b", "--bible-file", default=None, type=str,
+                help="Bible verse database file.")
+ap.add_argument("-d", "--debug", default=False, action="store_true",
+                help="DEBUG MODE: show edit list lines and verse references.")
+ap.add_argument("edit_list", type=str, metavar="EDITS_FILE", help="Reference edit list file.")
+args = ap.parse_args()
 
-bb = BibleBooks.fromfile()
+bb = BibleBooks.fromfile(args.bible_file or BIBLE_FILE)
 
-for line in sys.stdin:
-    line = line.strip()
-    if line.startswith("#"):
-        continue
-    for vr in parse_ref(line, bb):
-        print(bb[vr])
-    
+with open(args.edit_list, "rt", encoding="utf8") as fd:
+    for i, line in enumerate(fd):
+        line = line.strip()
+        if args.debug:
+            print(f"{args.edit_list}:{i+1}: {line}")
+        if line.startswith("#"):
+            continue
+        for vr in parse_ref(line, bb):
+            if args.debug:
+                print(vr, end="\t")
+            print(bb[vr])
+        
